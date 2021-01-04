1490992cadca287ef260f878a0baf9df8c27257b
Igor Asamadzinau <igor.asamadzinau@fmr.com>
2021-01-04 18:47:22 -0500

    Rollback to init status on master branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { Exer1Component } from './exer1/exer1.component';
-import { Exer2Component } from './exer2/exer2.component';
-import { Exer3Module} from './exer3/exer3.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Exer1Component,
-    Exer2Component
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    Exer3Module
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
